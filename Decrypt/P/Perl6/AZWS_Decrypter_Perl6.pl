5956914663a81816c7c4e8c34b0c0a3a35a7e84e
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-23 22:26:17 -0800

    AZWS_Decrypter_Perl6.pl
    Version 1 of the AZWS decrypter, written in Perl 6.

---
+++ Decrypt/P/Perl6/AZWS_Decrypter_Perl6.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/env perl
+# Script goes below here
+
+"""
+Take this section out and write your own decrypter here.
+"""
+
+# Script goes above here
+# V1 - 2022 Wednesday February 23rd at 10:26 pm
