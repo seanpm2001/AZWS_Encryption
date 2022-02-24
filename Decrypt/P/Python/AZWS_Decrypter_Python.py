24de378fbd8026f0c9c432bf433f1bbb9cda2f03
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-23 22:24:06 -0800

    AZWS_Decrypter_Python.py [V1]
    Version 1 of the Python AZWS decryption script.

---
+++ Decrypt/P/Python/AZWS_Decrypter_Python.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# Script goes below here
+
+"""
+Take this section out and write your own decrypter here.
+"""
+
+# Script goes above here
+# V1 - 2022Wednesday February 23rd at 10:24 pm
