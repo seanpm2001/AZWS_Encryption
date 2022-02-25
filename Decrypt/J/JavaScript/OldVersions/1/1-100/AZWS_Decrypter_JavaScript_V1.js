4af0f1687965857d568a71e11c91badb27eb33ae
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-24 17:46:03 -0800

    AZWS_Decrypter_JavaScript_V1.js
    An archive of the first version of the AZWS Decrypter script, written in JavaScript.

---
+++ Decrypt/J/JavaScript/OldVersions/1/1-100/AZWS_Decrypter_JavaScript_V1.js
@@ -0,0 +1,16 @@
+// JavaScript language
+// Script goes below here
+
+function AZWS_Decrypter_Main() {
+// SECTION
+      console.log("Take this section out and write your own decrypter here.");
+// SECTION
+}
+return AZWS_Decrypter_Main();
+console.log("AZWS Decryption complete!");
+alert("AZWS Decryption complete!");
+break;
+
+// Script goes above here
+// Current line count (including blank lines and compiler line): 17
+// V1 - 2022 Thursday February 24th at 5:45 pm
