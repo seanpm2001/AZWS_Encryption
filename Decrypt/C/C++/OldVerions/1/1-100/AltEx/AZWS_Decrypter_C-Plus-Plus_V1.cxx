0cb2929da752af02e38e5f05245fc42e8ad22af3
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-24 17:29:25 -0800

    AZWS_Decrypter_C-Plus-Plus_V1.cxx
    An archive of the first version of the C++ AZWS Decrypter script.

---
+++ Decrypt/C/C++/OldVerions/1/1-100/AltEx/AZWS_Decrypter_C-Plus-Plus_V1.cxx
@@ -0,0 +1,16 @@
+// C++ language
+// Script goes below here
+
+#include <iostream>
+int main()
+{
+// SECTION
+	std:cout << "Take this section out and write your own decrypter here.\n";
+// SECTION
+}
+return main();
+return 0;
+
+// Script goes above here
+// Current line count (including blank lines and compiler line): 17
+// V1 - 2022 Thursday February 24th at 5:28 pm
