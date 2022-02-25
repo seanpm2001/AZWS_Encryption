f461211dbeade71500c1043562a471ace1ac0718
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-24 17:34:27 -0800

    AZWS_Decrypter_Java_V1.java
    An archive of the first version of the AZWS Decrypter script, written in Java.

---
+++ Decrypt/J/Java/OldVersions/1/1-100/AZWS_Decrypter_Java_V1.java
@@ -0,0 +1,16 @@
+// Java language
+// Script goes below here
+
+public class AZWS_Decrypter_App {
+// SECTION
+    public static void main(String[] args) {
+      System.out.println("Take this section out and write your own decrypter here.");
+    }
+// SECTION
+}
+return main();
+break;
+
+// Script goes above here
+// Current line count (including blank lines and compiler line): 17
+// V1 - 2022 Thursday February 24th at 5:33 pm
