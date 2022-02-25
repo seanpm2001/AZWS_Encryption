3354164a588528cf7b9aa7daba62260c179defa9
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-24 18:07:09 -0800

    AZWS_Decrypter_Elm.elm [V1]
    Version 1 of the AZWS Decrypter script for the Elm programming language.

---
+++ Decrypt/E/Elm/AZWS_Decrypter_Elm.elm
@@ -0,0 +1,9 @@
+-- Elm language
+-- Script goes below here
+
+greeting =
+    "Take this section out and write your own decrypter here."
+
+-- Script goes above here
+-- Current line count (including blank lines and compiler line): 10
+-- V1 - 2022 Thursday February 24th at 6:07 pm
