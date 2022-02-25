de0f324d084f42d08237235159fdb4679b4b88fd
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-24 18:26:05 -0800

    AZWS_Decrypter_Dart.dart [V1]
    Version 1 of the AZWS Decryption script for the Dart programming language.

---
+++ Decrypt/D/Dart/AZWS_Decrypter_Dart.dart
@@ -0,0 +1,12 @@
+// Dart language
+// Script goes below here
+
+void main() {
+  print('Take this section out and write your own decrypter here.');
+}
+return main();
+break;
+
+// Script goes above here
+// Current line count (including blank lines and compiler line): 13
+// V1 - 2022 Thursday February 24th at 6:26 pm
