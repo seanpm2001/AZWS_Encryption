a93d4364926b5a86b24e71050ae99c2ff7dd6f48
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-24 17:24:45 -0800

    AZWS_Decrypter_C_V1.c
    An archive of the first version of the C AZWS decrypter script.

---
+++ Decrypt/C/C/OldVersions/1/1-100/AZWS_Decrypter_C_V1.c
@@ -0,0 +1,17 @@
+// C language
+// Script goes below here
+
+#include <stdio.h>
+int main(void) {
+
+// SECTION
+printf("Take this section out and write your own decrypter here.\n");
+// SECTION
+
+}
+return main();
+return 0;
+
+// Script goes above here
+// Current line count (including blank lines and compiler line): 18
+// V1 - 2022 Thursday February 24th at 5:24 pm
