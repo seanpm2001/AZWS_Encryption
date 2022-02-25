ca3f7d9528e9edeedcb8a6d9d326a76c155b6caa
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-24 18:04:02 -0800

    AZWS_Decrypter_Go!.go [V1]
    Version 1 of the AZWS Decrypter script for the Go! programming language.

---
+++ Decrypt/G/Go!/AZWS_Decrypter_Go!.go
@@ -0,0 +1,8 @@
+// Go! language
+// Script goes below here
+
+// Take this section out and write your own decrypter here.")
+
+// Script goes above here
+// Current line count (including blank lines and compiler line): 9
+// V1 - 2022 Thursday February 24th at 6:04 pm
