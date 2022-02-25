4cad61d37fbae72af67996c607540d22c01e22a8
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-24 17:02:01 -0800

    AZWS_Decrypter_Scala.scala [V1]
    Version 1 of the AZWS Decrypter script for Scala.

---
+++ Decrypt/S/Scala/AZWS_Decrypter_Scala.scala
@@ -0,0 +1,9 @@
+#!/usr/bin/env scala
+// Script goes below here
+
+/*
+// Take this section out and write your own decrypter here.
+*/
+
+// Script goes above here
+// V1 - 2022 Thursday February 24th at 5:02 pm
