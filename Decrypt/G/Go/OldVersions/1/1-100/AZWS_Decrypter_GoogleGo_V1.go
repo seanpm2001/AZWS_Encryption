a90b264cbe0914f99a00beface7fce210beb833b
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-24 18:02:50 -0800

    AZWS_Decrypter_GoogleGo_V1.go
    An archive of the first version of the AZWS Decrypter script for the Google Go programming language.

---
+++ Decrypt/G/Go/OldVersions/1/1-100/AZWS_Decrypter_GoogleGo_V1.go
@@ -0,0 +1,14 @@
+// Google Go language
+// Script goes below here
+
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Take this section out and write your own decrypter here.")
+}
+
+// Script goes above here
+// Current line count (including blank lines and compiler line): 15
+// V1 - 2022 Thursday February 24th at 6:02 pm
