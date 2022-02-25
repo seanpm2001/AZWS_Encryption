e321c279b9c84d70a4b06082e944fe3abfd4e408
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-24 17:08:47 -0800

    AZWS_Decrypter_Batchfile_V1.bat
    An archive of the first version of the Batchfile AZWS Decryption script.

---
+++ Decrypt/B/Batchfile/OldVersions/1/1-100/AZWS_Decrypter_Batchfile_V1.bat
@@ -0,0 +1,9 @@
+REM Batchfile language
+REM Script goes below here
+
+::
+REM Take this section out and write your own decrypter here.
+::
+
+REM Script goes above here
+REM V1 - 2022 Thursday February 24th at 5:08 pm
