4e8a1ae7eadb89b5b0ac891d0620196ceccbcd6d
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-24 19:54:02 -0800

    AZWS_Encryptor_Perl.pl [V1]
    Version 1 of the AZWS encryption file for this project.

---
+++ Encryption/P/Perl6/AZWS_Encryptor_Perl.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/env perl
+# Start of script
+'''
+About:Encryption
+We are now going to do the opposite, and encrypt a document with the AZWS typing discipline.
+If done successfully, it should follow the criteria for an AZWS encrypted document, and be capable of being decrypted by any of the provided decryptors.
+'''
+# Script goes below here
+
+# Import section
+import random
+import os
+
+"""
+Take this section out and write your own decrypter here.
+"""
+
+# Script goes above here
+# Line count (including blank lines and compiler line): 22
+# V1 - 2022 Thursday February 24th at 7:54 pm
+# End of script
