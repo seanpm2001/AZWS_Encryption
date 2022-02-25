d35020aca0be5733becdb5f6e18fc7b9f111a718
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-24 17:54:02 -0800

    AZWS_Decrypter_PureScript.purs [V1]
    Version 1 of the AZWS Decryption script for PureScript.

---
+++ Decrypt/P/PureScript/AZWS_Decrypter_PureScript.purs
@@ -0,0 +1,16 @@
+-- PureScript language
+-- Script goes below here
+
+module AZWS_Decrypt_Main where
+
+import Prelude
+
+import Effect (Effect)
+import Effect.Console (log)
+
+AZWS_Decrypt_Main  :: Effect Unit
+AZWS_Decrypt_Main  = log "Take this section out and write your own decrypter here."
+
+-- Script goes above here
+-- Current line count (including blank lines and compiler line): 17
+-- V1 - 2022 Thursday February 24th at 5:54 pm
