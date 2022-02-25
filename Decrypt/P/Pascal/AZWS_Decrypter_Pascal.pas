bf030d53bf8946809e9b8595250a69d573180330
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-24 17:49:01 -0800

    AZWS_Decrypter_Pascal.pas [V1]
    Version 1 of the AZWS Decrypter script, written in Pascal.

---
+++ Decrypt/P/Pascal/AZWS_Decrypter_Pascal.pas
@@ -0,0 +1,13 @@
+// Pascal language
+// Script goes below here
+
+program AZWS_Decrypter(output);
+begin
+{ SECTION }
+      Write('Take this section out and write your own decrypter here.')
+{ SECTION }
+end.
+
+// Script goes above here
+// Current line count (including blank lines and compiler line): 14
+// V1 - 2022 Thursday February 24th at 5:49 pm
