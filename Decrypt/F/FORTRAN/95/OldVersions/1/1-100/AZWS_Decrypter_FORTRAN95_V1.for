5d3e3d140eba3405b8d01c7bcebb9d5d781231a0
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-24 18:14:46 -0800

    AZWS_Decrypter_FORTRAN95_V1.for
    An archive of the first version of the AZWS Decryption script, written in FORTRAN 95.

---
+++ Decrypt/F/FORTRAN/95/OldVersions/1/1-100/AZWS_Decrypter_FORTRAN95_V1.for
@@ -0,0 +1,10 @@
+C Fortran language
+C Script goes below here
+
+program AZWS_Decrypt
+     print *, "Take this section out and write your own decrypter here."
+end program AZWS_Decrypt
+
+C Script goes above here
+C Current line count (including blank lines and compiler line): 11
+C V1 - 2022 Thursday February 24th at 6:14 pm
